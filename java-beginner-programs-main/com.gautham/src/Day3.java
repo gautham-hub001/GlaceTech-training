519b72e3c6f1fd95d2ab7babf831ecd83d40ba80
gautham-hub001 <gautham2pothana@gmail.com>
2023-04-10 21:45:06 -0400

    intial commit

--- java-beginner-programs-main/com.gautham/src/Day3.java
+++ java-beginner-programs-main/com.gautham/src/Day3.java
@@ -1,7 +1,9 @@
 public class Day3 {
+    int a = 10;
+
     public static void main(String[] args) {
         // static variables
         // static keyword members gets instantiated
-        System.out.println(Constants.PI);
+        System.out.println(Constants.a);
     }
 }
--- java-beginner-programs-main/src/Constants.java
+++
@@ -1,3 +0,0 @@
-public class Constants {
-    static float PI = 3.14f;
-}
