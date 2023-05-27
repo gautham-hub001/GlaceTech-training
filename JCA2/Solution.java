b8ce56f2d6643a0b46acbbbd31f4bee9cbe6f0db
gautham-hub001 <83133897+gautham-hub001@users.noreply.github.com>
2023-05-27 13:27:29 -0400

    JCA2 till 4th

--- JCA2/Solution.java
+++ JCA2/Solution.java
@@ -6,14 +6,32 @@ import java.time.Period;
 public class Solution {
     public static void main(String[] args) {
         List<Employee> employees = Employees.getEmployees();
-        Map<Integer, Employee> employeesMap = new HashMap();
 
-        employeesMap = employees.stream()
-                .collect(Collectors.toMap(e -> e.getEin(), e -> e, (oldValue, newValue) -> newValue));
-        // System.out.println(employeesMap);
+        // 4. Map<Integer, List<Employee>> ein, list of employees -> last field in employees.txt is company id
+        Map<Integer, List<Employee>> employeesMap = new HashMap();
+        
+       employeesMap = employees.stream()
+                            .collect(Collectors.toMap(e->e.getEin(), e-> {List<Employee> emp = new ArrayList<Employee>(); emp.add(e); return emp;}, (oldValue, newValue) -> {
+                                List<Employee> emp = new ArrayList<Employee>();
+                                if(oldValue instanceof Employee) { 
+                                emp.add(oldValue.get(0));
+                                emp.add(newValue.get(0));
+                                return emp;
+                            }
+                            else{ // instanceof List<Employee>
+                                oldValue.add(newValue.get(0));
+                                return oldValue;
+                            }
+                            
+                        }));
+        for(Map.Entry<Integer,List<Employee>> e: employeesMap.entrySet()) {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+            System.out.println();
+        }
+         
 
         List<Company> companies = Companies.getCompanies();
-
         Map<Integer, String> companiesMap = new HashMap();
         companiesMap = companies.stream()
                 .collect(Collectors.toMap(e -> e.getEin(), e -> e.getCompanyName(), (oldValue, newValue) -> newValue));
@@ -47,9 +65,30 @@ public class Solution {
         // c. [companyName, ein, firstName, lastName, email, employeeId] - sorted by
         // FirstName
         Collections.sort(employees, (o1, o2) -> o1.getEmployeeId().compareTo(o2.getEmployeeId()));
-        for (Employee e : employees) {
-            System.out.println("[ " + e.getEin() + ", " + e.getFirstName() + ", " + e.getLastName() + ", "
-                    + e.getEmail() + ", " + e.getEmployeeId() + " ]");
-        }
+        // for (Employee e : employees) {
+        //     System.out.println("[ " + e.getEin() + ", " + e.getFirstName() + ", " + e.getLastName() + ", "
+        //             + e.getEmail() + ", " + e.getEmployeeId() + " ]");
+        // }
     }
-}
+
+    
+
+     
+
+    
+    
+            }
+            
+            
+        
+    
+
+    
+    
+
+        
+
+    
+
+
+    
