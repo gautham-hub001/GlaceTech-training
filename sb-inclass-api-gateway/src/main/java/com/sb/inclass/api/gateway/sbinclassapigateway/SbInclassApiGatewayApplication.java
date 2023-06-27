822280abdc787dd7323ed0e7666a62f811cff7ce
gautham-hub001 <83133897+gautham-hub001@users.noreply.github.com>
2023-06-26 20:47:45 -0400

    api gateway

---
+++ sb-inclass-api-gateway/src/main/java/com/sb/inclass/api/gateway/sbinclassapigateway/SbInclassApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sb.inclass.api.gateway.sbinclassapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbInclassApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbInclassApiGatewayApplication.class, args);
+	}
+
+}
